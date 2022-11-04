5fd56f5990ff2a1067ca21fd4e2dda32263b0bc5
Omid-Neshati <tchandnofame@gmail.com>
2022-11-04 19:02:59 +0330

    add translation

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,18 +1,24 @@
+import { GetStaticProps } from "next";
+import { useTranslations } from "next-intl";
 import React from "react";
 import { Link as ScrollLink } from "react-scroll";
 
 function Hero() {
+  const t = useTranslations("homePage.hero");
+
   return (
     <>
       <div
-        className="relative heroElem w-full pt-20 pb-40 m-auto flex justify-center text-center flex-col items-center z-1"
+        className="relative flex flex-col items-center justify-center w-full pt-20 pb-40 m-auto text-center heroElem z-1"
         style={{ maxWidth: "1200px" }}
       >
-        <p className="text-xl mb-5">Hey, I'm Brayden.</p>
-        <h1 className="heroTitle inline-block max-w-2xl lg:max-w-4xl  w-auto relative text-5xl md:text-6xl lg:text-7xl tracking-tighter mb-10 font-bold heroShinyBg">
-          I enjoy <span className="heroShiny1 text-fun-pink">building</span> and{" "}
-          <span className="heroShiny2 text-fun-pink">designing</span> for the
-          web.
+        <p className="mb-5 text-xl">{t("title")}</p>
+        <h1 className="relative inline-block w-auto max-w-2xl mb-10 text-5xl font-bold tracking-tighter heroTitle lg:max-w-4xl md:text-6xl lg:text-7xl heroShinyBg">
+          {t("description.1")}{" "}
+          <span className="heroShiny1 text-fun-pink">{t("description.2")}</span>{" "}
+          {t("description.3")}{" "}
+          <span className="heroShiny2 text-fun-pink">{t("description.4")}</span>{" "}
+          {t("description.5")}{" "}
           <img
             className="sqD squiggle-hero-html w-16 top-[-90px] right-[5%] sm:top-[-90px] sm:right-[170px]"
             style={{ animationDelay: "0.1s" }}
@@ -66,8 +72,8 @@ function Hero() {
           smooth={true}
           duration={500}
         >
-          <div className="cursor-pointer font-bold whitespace-nowrap px-10 py-4 text-fun-white border-2 text-xl rounded-full border-fun-white bg-bg hover:bg-fun-pink hover:text-white hover:border-fun-pink transition-colors">
-            Tell me more
+          <div className="px-10 py-4 text-xl font-bold transition-colors border-2 rounded-full cursor-pointer whitespace-nowrap text-fun-white border-fun-white bg-bg hover:bg-fun-pink hover:text-white hover:border-fun-pink">
+            {t("scrollLink")}
           </div>
         </ScrollLink>
       </div>
