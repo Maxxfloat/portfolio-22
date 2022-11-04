ebee58417a51cee229ac287dbf54e92886274668
Omid-Neshati <tchandnofame@gmail.com>
2022-11-04 19:14:25 +0330

    import and add translation and fix direction

--- pages/projects/tag/[tag].tsx
+++ pages/projects/tag/[tag].tsx
@@ -1,34 +1,50 @@
 import Page from "components/utility/Page";
 
 import { GetStaticProps, GetStaticPaths } from "next";
-import { allKebabTags, allTags } from "@/data/content/projects";
-
-import projects from "@/data/content/projects";
+import { projects } from "@/data/global";
 
 import { kebabCase, kebabArray } from "@/utils/utils";
 import Projects from "components/projects/Projects";
 import Heading from "components/projects/Heading";
-import More from "components/projects/More";
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
-export const getStaticPaths: GetStaticPaths = async () => {
+export const allTags = [];
+
+projects.forEach((project) => {
+  project.tags.forEach((tag) => !allTags.includes(tag) && allTags.push(tag));
+});
+
+export const allKebabTags = allTags.map((tag) => kebabCase(tag));
+
+export const getStaticPaths: GetStaticPaths = async ({ locales }) => {
   const allTags = [];
   projects.forEach((project) =>
     project.tags.forEach((tag) => {
       allTags.push(tag);
     })
   );
-  const uniqueAllTags = [...new Set(allTags)];
-  const allTagsPaths = uniqueAllTags.map((path) => ({
-    params: { tag: `${kebabCase(path)}` },
-  }));
+  const allTagsPaths = allTags
+    .map((path) =>
+      locales.map((locale) => ({
+        params: { tag: `${kebabCase(path)}` },
+        locale,
+      }))
+    )
+    .flat();
   return {
     paths: allTagsPaths,
     fallback: false,
   };
 };
 
-export const getStaticProps: GetStaticProps = async ({params}: {params: {tag: string}}) => {
+export const getStaticProps: GetStaticProps = async ({
+  params,
+  locale,
+}: {
+  params: { tag: string };
+  locale: string;
+}) => {
   const tag = params.tag;
   const filteredProjects = projects.filter((project) =>
     [...kebabArray(project.tags)].includes(tag)
@@ -37,31 +53,30 @@ export const getStaticProps: GetStaticProps = async ({params}: {params: {tag: st
     props: JSON.parse(
       JSON.stringify({
         filteredProjects,
-        tag: tag,
+        tag,
+        messages: (await import(`../../../data/locales/${locale}.json`))
+          .default,
       })
     ),
   };
 };
 
 function PostPage({ filteredProjects, tag }) {
+  const t = useTranslations("homePage.projects");
   const capsTag = allTags[allKebabTags.indexOf(tag)];
   return (
     <Page
       currentPage="Projects"
-      meta={{
-        title: `${capsTag} Projects`,
-        desc: `A showcase for all of my ${capsTag} projects.`,
-      }}
+      desc={`A showcase for all of my ${capsTag} projects.`}
     >
       <Heading tag={capsTag} />
       <Projects overwriteProjects={filteredProjects} />
 
       <Link href="/projects">
-        <div className="mt-8 max-w-sm md:max-w-2xl border border-fun-pink mx-auto text-center w-full whitespace-nowrap px-8 py-3 rounded-full text-fun-pink bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white transition-colors cursor-pointer">
-          View All
+        <div className="w-full max-w-sm px-8 py-3 mx-auto mt-8 text-center transition-colors border rounded-full cursor-pointer md:max-w-2xl border-fun-pink whitespace-nowrap text-fun-pink bg-fun-pink-darkerer hover:bg-fun-pink hover:text-white">
+          {t("link")}
         </div>
       </Link>
-      {/* <More /> */}
     </Page>
   );
 }
